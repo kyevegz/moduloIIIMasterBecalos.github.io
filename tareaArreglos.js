c03dc1bee7eee2c0ad7d206744a15186ff40b4b6
kyevegz <jimenacarrillocastillo@gmail.com>
2026-03-01 22:40:29 -0600

    tarea de arreglos jeje

--- tareaArreglos.js
+++ tareaArreglos.js
@@ -1,3 +1,30 @@
-let frutas = ["mango", "fresas", "frambuesas", 
-    "moras", "cerezas", "melon", "manzana", "jicama"];
+let frutas = ["mango", "melon", "mango", "fresa", "frambuesa", "jicama", "jicama", "fresa", "arandano",
+    "mora", "arandano", "cereza", "melon", "manzana", "frambuesa", "jicama", "arandano", "manzana", "frambuesa", "fresa", "arandano"];
+
+const contadorFrutas = {
+    mango: 0,
+    fresa: 0,
+    frambuesa: 0,
+    mora: 0,
+    cereza: 0,
+    melon: 0,
+    manzana: 0,
+    jicama: 0,
+    arandano: 0
+};
+
+const arrayPropiedades = Object.keys(contadorFrutas);
+
+for(let i = 0; i < frutas.length; i++){
+    for(let j = 0; j < arrayPropiedades.length; j++){
+        if(frutas[i] === arrayPropiedades[j]){
+            contadorFrutas[arrayPropiedades[j]] += 1;
+        }
+    }
+}
+
+for(let i = 0; i < arrayPropiedades.length; i++){
+    console.log(arrayPropiedades[i] + ": " + contadorFrutas[arrayPropiedades[i]]);
+}
+
 
