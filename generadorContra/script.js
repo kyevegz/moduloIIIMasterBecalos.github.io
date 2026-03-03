c91a0c02ebb1b25d385b705545fd804ddfb0e340
kyevegz <jimenacarrillocastillo@gmail.com>
2026-03-03 00:13:03 -0600

    generador de contra

--- generadorContra/script.js
+++ generadorContra/script.js
@@ -6,73 +6,144 @@ checkMayus = document.getElementById('chbMayus'),
 checkMinus = document.getElementById('chbMinus'),
 checkNumber = document.getElementById('chbNumbers'),
 checkSymbol = document.getElementById('chbSymbol'),
-formulario = document.getElementById('form-password');
-let mayus = false, minus = false, numero = false, simbolo = false,
-contador = 0, arrayPosibles = [];
+formulario = document.getElementById('form-password'),
+txtContra = document.getElementById('txtcopy'),
+btnEnv = document.getElementById('copy'),
+checkeados = {
+    mayus: false,
+    minus: false,
+    numero: false,
+    simbolo: false
+};
+let contador = 0, arrayPosibles = [];
+
+
+btnEnv.addEventListener("click", function(){
+    const txt = document.getElementById("txtcopy");
+    txt.select();
+    document.execCommand('copy');
+})
+
 
 document.addEventListener("DOMContentLoaded", function(){
     range.value = 8;
+    spanValor.textContent = range.value;
 });
 range.addEventListener('input', (event)=>{
     spanValor.textContent = range.value;
     
 });
 
-checkMayus.addEventListener('change', (event) =>{
-    if(checkMayus.checked){
-        mayus = true;
-        console.log("Mayúscula");
-    }else{
-        mayus = false;
-        console.log("Desmarcado");
-    }
-    
-});
 
-checkMinus.addEventListener('change', (event) =>{
-    if(checkMinus.checked){
-        minus = true;
-        console.log("minus");
-    }else{
-        minus = false;
-        console.log("Desmarcado");
-    }
-    
-});
-
-checkNumber.addEventListener('change', (event) =>{
-    if(checkNumber.checked){
-        numero = true;
-        console.log("number");
-    }else{
-        numero = false;
-        console.log("Desmarcado");
-    }
-    
-});
-
-checkSymbol.addEventListener('change', (event) =>{
-    if(checkSymbol.checked){
-        simbolo = true;
-        console.log("symbol");
-    }else{
-        simbolo = false;
-        console.log("Desmarcado");
-    }
-    
-});
 
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
+
+        arrayPosibles = [];
+        if(checkMayus.checked){
+            checkeados.mayus = true;
+            console.log("Mayúscula");
+        }else{
+            checkeados.mayus = false;
+            console.log("Desmarcado");
+        }
+        
+        if(checkMinus.checked){
+            checkeados.minus = true;
+            console.log("minus");
+        }else{
+            checkeados.minus = false;
+            console.log("Desmarcado");
+        }
+ 
+        if(checkNumber.checked){
+            checkeados.numero = true;
+            console.log("number");
+        }else{
+            checkeados.numero = false;
+            console.log("Desmarcado");
+        }
+        
+   
+        if(checkSymbol.checked){
+            checkeados.simbolo = true;
+            console.log("symbol");
+        }else{
+            checkeados.simbolo = false;
+            console.log("Desmarcado");
+        }
+        
+   
+
+
     contador = range.value;
-    if(mayus){
-        arrayPosibles
-        console.log("jai");
+    let indice = 0, randomOpciones = 0, swInd = 0, rdmLSpace = 0, 
+    contraFinal = "";
+
+    for(let i in checkeados){
+        if(checkeados[i] == true){
+            arrayPosibles.push(indice); 
+        }
+        // if(checkeados.hasOwnProperty(i)){
+        //     console.log(i + " " + checkeados[i]);
+        // }
+        indice++;
+        
     }
 
-    while(contador != 0){
+    
+    //console.log(randomOpciones);
+    
 
+    while(contador != 0){  
+        
+        randomOpciones = Math.floor(Math.random() * arrayPosibles.length);
+        
+        // for(let j in arrayPosibles){
+        //     if(arrayPosibles[j] == randomOpciones){
+        //         swInd = randomOpciones;
+        //     }
+        // }
+        //console.log(swInd);
+        swInd = arrayPosibles[randomOpciones];
+        switch(swInd){
+            case 0:
+                rdmLSpace = Math.floor(Math.random() * 26); 
+                contraFinal += letras(rdmLSpace).toUpperCase();
+                console.log(letras (rdmLSpace).toUpperCase());
+                break;
+            case 1:
+                rdmLSpace = Math.floor(Math.random() * 26)
+                contraFinal += letras(rdmLSpace);
+                console.log(letras (rdmLSpace));
+                break;
+            case 2:
+                rdmLSpace = Math.floor(Math.random() * 9);
+                contraFinal += rdmLSpace.toString();
+                console.log(rdmLSpace);
+                break;
+            case 3:
+                rdmLSpace = Math.floor(Math.random() * 16);
+                contraFinal += simbolos(rdmLSpace);
+                console.log(simbolos(rdmLSpace));
+                break;
+        }
         contador--;
     }
-    
+    checkeados.mayus = false;
+    checkeados.minus = false;
+    checkeados.numero = false;
+    checkeados.simbolo = false;
+    btnEnv.disabled = false;
+    txtContra.value = contraFinal;
 });
+
+function letras(rdm){
+    const abecedario = "abcdefghijklmnopqrstuvwxyz";
+    return abecedario[rdm];
+}
+
+function simbolos(rad){
+    const symbols = "*/_-+#$%&=)([]}{";
+    return symbols[rad];
+}
